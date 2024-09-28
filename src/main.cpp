077c7bd3fb09139ea6136361193d30b65a551227
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-09-28 15:11:52 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Minecraft", 1280, 720, SDL_WINDOW_VULKAN);
+    SDL_Window* window = SDL_CreateWindow("Minecraft - vulkan", 1280, 720, SDL_WINDOW_VULKAN);
 
     SDL_UpdateWindowSurface(window);
     SDL_Delay(1000);
